4df1a2a88733a8e517218cb3c2e848f63a9fbd4d
임승현 <96limshyun@gmail.com>
2025-06-02 16:04:05 +0900

    Chore: husky install 및 커밋 시 적용 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
+import globals from "globals";
 
 export default defineConfig([
-  { files: ["**/*.js"], plugins: { js }, extends: ["js/recommended"] },
-
   {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: { js },
+    extends: ["js/recommended"],
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
