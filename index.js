3923311de7d761a93ce6074e2647f83cc88dd222
임승현 <96limshyun@gmail.com>
2025-06-02 17:16:46 +0900

    Chore: express 서버 초기 설정 및 dotenv를 통한 환경변수 구성, cors/body-parser 미들웨어 추가, nodemon 개발용 설정

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const a = 1;
-console.log(1);
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.EXPRESS_PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ App listening on port ${PORT}`);
+});
