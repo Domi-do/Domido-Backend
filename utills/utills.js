2d1e5d43a08259346515b79c443d7259ae391450
moonin1228 <80632139+moonin1228@users.noreply.github.com>
2025-06-06 00:59:44 +0900

    Feature: 로그인 기능 구현 (카카오 OAuth) (#7)
    - Feature: MongoDB 사용자 스키마(auth) 정의
    - Chore: jsonwebtoken 및 cookie-parser 미들웨어 도입
    - Feature: 카카오 로그인 기능 구현
    - Refactor: 코드 리뷰 반영

---
+++ utills/utills.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+
+export function generateAccessToken(payload) {
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "10m" });
+}
+
+export function generateRefreshToken(payload) {
+  return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
+}
