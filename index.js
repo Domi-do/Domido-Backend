e1b791cbe793d275ae90c96ef9b9b57d3c808c64
moonin1228 <moonin1228@gmail.com>
2025-06-05 19:29:14 +0900

    Chore: jsonwebtoken 및 cookie-parser 미들웨어 도입
    #6

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
 import dominosRouter from "./routes/dominos.js";
+import authRouter from "./routes/auth.js";
 
 dotenv.config();
 
@@ -15,13 +16,15 @@ const app = express();
 await connectDB();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
 app.use("/dominos", dominosRouter);
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`✅ App listening on port ${PORT}`);
