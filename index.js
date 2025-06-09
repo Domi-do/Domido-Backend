dc0b7ca30793042a57d1b94ea86d2abec729cec2
moonin1228 <80632139+moonin1228@users.noreply.github.com>
2025-06-09 19:21:55 +0900

    Feature: 리프레시 토큰을 이용한 액세스 토큰 재발급 API 구현 (#19)
    * Refactor: cookie-parser 미사용으로 인해 제거
    * Feature: 로그아웃 라우터 추가 및 DB에 토큰 정보 저장 기능 구현

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
 import dominosRouter from "./routes/dominos.js";
@@ -23,7 +22,6 @@ app.use(
   }),
 );
 app.use(express.json());
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("hello");
