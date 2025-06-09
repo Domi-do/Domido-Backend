d562931675a2db4671a9bd43e0628366af2c010e
임승현 <96limshyun@gmail.com>
2025-06-09 21:15:20 +0900

    Refactor: 환경변수를 사용해 origin 설정 하드코딩 제거
    #20

--- config/socket.js
+++ config/socket.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 const socketSetup = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONT_END_URL,
       methods: ["GET", "POST"],
     },
   });
