a51d4230be24adaffffec300b87cc30f7de4374a
eunseozz <eunseolovely@gmail.com>
2025-06-18 22:05:53 +0900

    Feature: 프리셋 프로젝트 생성 시 기본 도미노 데이터 자동 삽입 기능 추가
    - presetDominos.js 파일 생성하여 도미노 템플릿 정의
    - createPresetProjects 로직에 각 프로젝트별 프리셋 도미노 삽입 처리 추가
    - insertDominos 유틸 함수 구현하여 DB에 도미노 일괄 저장 가능하도록 처리
    
    #38

--- utills/utills.js
+++ utills/utills.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 
 import User from "../Models/UserInfoSchema.js";
+import { ProjectModel } from "../Models/ProjectSchema.js";
+import { DominoModel } from "../Models/DominosSchema.js";
 
 export const generateAccessToken = (payload) =>
   jwt.sign({ ...payload, tokenType: "access" }, process.env.JWT_SECRET, {
@@ -23,5 +25,37 @@ export const refreshAccessToken = async (refreshToken) => {
     return newAccessToken;
   } catch (error) {
     throw new Error(`토큰 재발급 실패: ${error.message}`);
+  }
+};
+
+export const createProject = async ({ title, ownerId }) => {
+  try {
+    const newProject = await ProjectModel.create({
+      title,
+      ownerId,
+    });
+
+    return newProject;
+  } catch (error) {
+    console.error("프로젝트 생성 중 에러 발생:", error);
+    throw error;
+  }
+};
+
+export const insertDominos = async (projectId, dominos) => {
+  if (!projectId || !Array.isArray(dominos)) {
+    throw new Error("유효하지 않은 projectId 또는 도미노 배열입니다.");
+  }
+
+  const dominosWithProjectId = dominos.map((domino) => ({
+    ...domino,
+    projectId,
+  }));
+
+  try {
+    await DominoModel.insertMany(dominosWithProjectId);
+  } catch (err) {
+    console.error("도미노 삽입 실패:", err);
+    throw err;
   }
 };
